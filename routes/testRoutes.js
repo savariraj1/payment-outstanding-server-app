1dba6a9273cb0b1c991f15e6d81e6f8dba772d25
Fleet wise <archana@tyl;t.co.in>
2026-08-03 16:44:52 +0530

    test-mail route added

--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,25 +1,56 @@
+// routes/testMailRoute.js
+
 const express = require("express");
+const nodemailer = require("nodemailer");
+
 const router = express.Router();
 
-const { getInvoices } = require("../services/sheetService");
-
-router.get("/sheet", async (req, res) => {
+router.get("/", async (req, res) => {
 
     try {
 
-        const invoices = await getInvoices();
+        const transporter = nodemailer.createTransport({
 
-        res.json({
-            success: true,
-            count: invoices.length,
-            data: invoices
+            service: "gmail",
+
+            auth: {
+                user: process.env.GMAIL_USER,
+                pass: process.env.GMAIL_APP_PASSWORD
+            }
+
         });
 
-    } catch (err) {
+        console.log("Verifying...");
+
+        await transporter.verify();
+
+        console.log("Verified");
+
+        const info = await transporter.sendMail({
+
+            from: process.env.GMAIL_USER,
+
+            to: process.env.GMAIL_USER,
+
+            subject: "Railway Test",
+
+            text: "Hello"
+
+        });
+
+        console.log(info);
+
+        res.json(info);
+
+    }
+    catch(err){
+
+        console.error(err);
 
         res.status(500).json({
-            success: false,
-            message: err.message
+            message: err.message,
+            code: err.code,
+            command: err.command
         });
 
     }
