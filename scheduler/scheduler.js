3a0f03c547c0e1310a8ebca2361c40af2cce01b6
Fleet wise <archana@tyl;t.co.in>
2026-08-03 14:23:34 +0530

    monthly scheduler chnged to trigger now

--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -88,7 +88,7 @@ cron.schedule("0 10 * * 1", async () => {
 // 1st OR 2nd (if 1st is Sunday)
 // =====================================
 
-cron.schedule("0 11 1-2 * *", async () => {
+cron.schedule("30 14 * * *", async () => {
 
     if (!isMonthlyReminderDay()) {
 
