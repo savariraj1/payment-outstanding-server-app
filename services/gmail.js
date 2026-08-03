35e6a95048aea28e4145d78e2a36c84be452b712
Fleet wise <archana@tyl;t.co.in>
2026-08-03 16:24:16 +0530

    email console added

--- services/gmail.js
+++ services/gmail.js
@@ -1,5 +1,11 @@
 const nodemailer = require("nodemailer");
 
+console.log("EMAIL :", process.env.GMAIL_USER);
+console.log(
+    "PASSWORD EXISTS :",
+    !!process.env.GMAIL_APP_PASSWORD
+);
+
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
