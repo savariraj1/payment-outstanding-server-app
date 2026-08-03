53d3617f5e9b53f9dbbff755f563102cb8db7314
Fleet wise <archana@tyl;t.co.in>
2026-08-03 17:02:46 +0530

    ipv6 issue resolving code

--- services/gmail.js
+++ services/gmail.js
@@ -1,10 +1,20 @@
+const dns = require("dns");
 const nodemailer = require("nodemailer");
 
+// Force Node.js to prefer IPv4 over IPv6
+dns.setDefaultResultOrder("ipv4first");
+
+// Check DNS resolution
+dns.lookup("smtp.gmail.com", { all: true }, (err, addresses) => {
+    if (err) {
+        console.log("DNS Lookup Error:", err);
+    } else {
+        console.log("SMTP Addresses:", addresses);
+    }
+});
+
 console.log("EMAIL :", process.env.GMAIL_USER);
-console.log(
-    "PASSWORD EXISTS :",
-    !!process.env.GMAIL_APP_PASSWORD
-);
+console.log("PASSWORD EXISTS :", !!process.env.GMAIL_APP_PASSWORD);
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -14,7 +24,7 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-transporter.verify(function (error, success) {
+transporter.verify((error, success) => {
     if (error) {
         console.log("SMTP Verify Error:");
         console.log(error);
