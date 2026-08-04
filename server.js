b194491bcc1381d410e2781fe9c589f7b6a702bf
Fleet wise <archana@tyl;t.co.in>
2026-08-04 17:13:27 +0530

    server testing mail removed

--- server.js
+++ server.js
@@ -13,44 +13,11 @@
     //     startScheduler();
 
     // });
-const net = require("net");
-
-const socket = net.createConnection(
-    {
-        host: "smtp.gmail.com",
-        port: 465
-    },
-    () => {
-        console.log("✅ Connected to Gmail SMTP");
-        socket.end();
-    }
-);
-
-socket.setTimeout(10000);
-
-socket.on("timeout", () => {
-    console.log("❌ Connection timed out");
-    socket.destroy();
-});
-
-socket.on("error", (err) => {
-    console.log("❌ Connection error:", err);
-});
-
-    
 require("dotenv").config();
-    
-    // console.log("DB_HOST:", process.env.DB_HOST);
-    // console.log("DB_USER:", process.env.DB_USER);
-    // console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-    // console.log("DB_NAME:", process.env.DB_NAME);
 
 const app = require("./app");
 const initDb = require("./config/initDb");
 
-// ===========================
-// Start Scheduler
-// ===========================
 console.log("================================");
 console.log("SERVER STARTING...");
 console.log("================================");
@@ -66,9 +33,7 @@ async function startServer() {
         app.listen(PORT, () => {
             console.log(`Server Running on Port ${PORT}`);
         });
-    }
-    catch (error) {
-        console.error("[SERVER] Startup failed");
+    } catch (error) {
         console.error(error);
         process.exit(1);
     }
